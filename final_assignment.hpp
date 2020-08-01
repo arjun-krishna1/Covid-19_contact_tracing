b06461e8d835a10e52edef1e54f8130cf7a817f0
arjun-krishna1 <arjunkrishna1306@gmail.com>
2020-08-01 10:58:38 -0400

    create classes to represent nodes and edges

--- final_assignment.hpp
+++ final_assignment.hpp
@@ -1,108 +1,60 @@
-#ifndef LAB3_BINARY_SEARCH_TREE_HPP
-#define LAB3_BINARY_SEARCH_TREE_HPP
+#ifndef FINAL_ASSIGNMENT
+#define FINAL_ASSIGNMENT
 
 #include <string>
+#include <vector>
 
 using namespace std;
 
 // PURPOSE: Represent a version of the Covid-19 contact tracing graph
-class ContactTracing {
+class ContactGraph
+{
 protected:
-	// PURPOSE: Models a single Task Item as a Tree Node
-	struct TaskItem {
-		int priority, avlBalance; // normal priority values are greater than zero
-		string description; // task description
-		TaskItem(int new_priority, string new_desc) :
-			priority(new_priority), avlBalance(0),
-			description(new_desc), left(NULL), right(NULL) {}
-		TaskItem* left;    // pointer to the left node
-		TaskItem* right;   // pointer to the right node
-		bool operator==(const TaskItem& rhs) {
-			bool are_equal = priority == rhs.priority;
-			// uncomment the next line if needed for the AVL implementation
-			/* are_equal = are_equal && avlBalance == rhs.avlBalance; */
-			return are_equal && description == rhs.description;
-		}
+	// PURPOSE: Represent an individual in a specific area
+	struct GraphNode
+	{
+		string id, name, date; // information about the individual and the date they were last tested
+		bool status;		   //infection status
+
+		// Default Constructor
+		GraphNode(string new_id, bool new_status) : id(new_id), status(new_status), name(""), date("") {}
 	};
 
-	friend class BinarySearchTreeTest;
+	//PURPOSE: Represent the most recsent contact between two individuals
+	struct GraphEdge
+	{
+		GraphNode *a, b;   //the two people who contacted each other
+		unsigned int time; //The number of days between the contact and the day it was reported
+		string loc;
 
-	// pointer to the root node of the tree	
-	TaskItem* root;
+		GraphEdge(GraphNode *new_a, GraphNode *new_b, unsigned int new_time) : a(new_a), b(new_b), time(new_time) {}
+	};
 
-	// number of nodes in the tree
-	unsigned int size;
+	// set of all nodes in graph
+	vector<GraphNode*> nodes;
+
+	// list of linked lists that specifies all edges that originate at each node
+	vector<vector<GraphEdge*>> edges;
+
+	// store the number of nodes and edges
+	unsigned int num_nodes, num_edges;
 
 public:
 	// CONSTRUCTOR AND DESTRUCTOR
 
 	// PURPOSE: Default/empty constructor
-	BinarySearchTree();
+	ContactGraph();
 
 	// PURPOSE: Explicit destructor of the class BinarySearchTree
-	~BinarySearchTree();
+	~ContactGraph();
 
-	// ACCESSORS  
+	// ACCESSORS
 	// PURPOSE: Returns the number of nodes in the tree
-	unsigned int get_size() const;
+	unsigned int get_num_nodes() const;
+	unsigned int get_num_edges() const;
 
-	// PURPOSE: Returns the maximum value of a node in the tree
-	// if the tree is empty, it returns (-1, "N/A")
-	TaskItem max() const;
-
-	// PURPOSE: Returns the minimum value of a node in the tree
-	// if the tree is empty, it returns (-1, "N/A")
-	TaskItem min() const;
-
-	// PURPOSE: Returns the tree height
-	unsigned int height() const;
-	unsigned int height_helper(TaskItem* root) const;
-
-	//PURPOSE: Recursive helper for the height function
-	// unsigned int height_helper(const TaskItem *r, unsigned int h){
-
-	// PURPOSE: Prints the contents of the tree; format not specified
-	void print() const;
-
-	//PURPOSE: Use recursion to print tree in pre-order
-	void pre_order_print_helper(TaskItem *r ) const;
-	// PURPOSE: Returns true if a node with the value val exists in the tree	
-	// otherwise, returns false
-	bool exists(TaskItem val) const;
-
-	// PURPOSE: Optional helper function that returns a pointer to the root node
-	TaskItem* get_root_node();
-
-	// PURPOSE: Optional helper function that returns the root node pointer address
-	TaskItem** get_root_node_address();
-
-	// PURPOSE: Optional helper function that gets the maximum depth for a given node
-	int get_node_depth(TaskItem* n) const;
-
-	// MUTATORS
-
-	// PURPOSE: Inserts the value val into the tree if it is unique
-	// returns true if successful; returns false if val already exists
-	bool insert(TaskItem val);
-
-	// PURPOSE: Removes the node with the value val from the tree
-	// returns true if successful; returns false otherwise
-	bool remove(TaskItem val);
-};
-
-class AVLTree : public BinarySearchTree {
-	// Define additional functions and attributes that you need below	
-
-public:
-	// PURPOSE: Overrides the BST insert function to keep the tree balanced
-	bool insert(TaskItem val);
-	// PURPOSE: Update the avl balance of all of the nodes
-	void updateNodeBalance(TaskItem* root);
-	// PURPOSE: Check if all the tree is balanced, if not conduct the necessary rotations to balance it
-	void balanceAVLTree(TaskItem* T, TaskItem* T_parent);
-	// PURPOSE: Rotate the tree right around T
-	void singleRightRotation(TaskItem* T, TaskItem* T_parent);
-	// PURPOSE: Rotate the tree left around T
-	void singleLeftRotation(TaskItem* T, TaskItem* T_parent);
+	// PURPOSE: traverse the graph starting at person_id and count the distinct individuals 
+	// who tested positive who are directly connected to the starting individual
+	int count_virus_positive_contacts(string person_id);
 };
 #endif
